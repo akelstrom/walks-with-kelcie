bf6a5e1ea97f4ba7a012cc6379c17d522a780cb3
akelstrom <akelstrom@gmail.com>
2022-08-11 18:40:33 -0400

    routing and mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { FaqComponent } from './faq/faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'faq', component: FaqComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { NavComponent } from './nav/nav.component';
+import { FaqComponent } from './faq/faq.component';
+import { Photo1Component } from './photo1/photo1.component';
+import { Photo2Component } from './photo2/photo2.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    NavComponent,
+    FaqComponent,
+    Photo1Component,
+    Photo2Component,
+    ParagraphComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule  ],
   providers: [],
   bootstrap: [AppComponent]
 })
